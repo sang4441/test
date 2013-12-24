9f756421eecfab7f5365430eca18971185b89362
sang <skim4441@gmail.com>
2013-12-24 13:26:05 -0500

    wip

--- js/views/workList/workView.js
+++ js/views/workList/workView.js
@@ -20,23 +20,27 @@ define([
       this.model = new WorkListModel();
       this.model.set('title', 'To do List');
       this.collection.add(this.model);
+
       // this.collection.bind("reset", this.render, this);
     },
 
     render: function(){
       var template = Handlebars.compile(WorkListTemplate);
-      var html = template(this.model.toJSON);
+      var html = template(this.model.attributes);
+            console.log(this.model.attributes);
+      console.log(this.model);
+
       this.$el.html(html);
       console.log("rendered");
     },
 
     addList: function() {
       console.log("add list");
-      this.model.set('title', 'Finished');
-      this.collection.add(this.model);
-      this.collection.each(function(model) {
-        // this.render();
-      });
+      // this.model.set('title', 'Finished');
+      // this.collection.add(this.model);
+      // this.collection.each(function(model) {
+      //   // this.render();
+      // });
     }
   });
 
